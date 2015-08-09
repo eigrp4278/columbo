02ca8e4d90f6fe8a0b9ccc67d2f4850d2c242283
eigrp4278 <eigrp4278@mail.com>
2015-08-09 20:09:21 +0000

    addded the public IP to the variables file

--- columbo.sh
+++ columbo.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # script that creates a all in one openstack node using a single phisycal NIC
-#
+# create the variables file
+# assign the IP for the non vagrant NAT interface
+echo export eth1_ip_addr=$(ip addr | grep 'inet '| grep -v '127.0.0.1' | grep -v '10.0.2.' | cut -d/ -f1 | awk '{ print $2}') >> variables
+
